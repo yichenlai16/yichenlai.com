d99f4a9a75a9806059939ef5dbe3a52ef89aafeb
yichenlai16 <63837651+yichenlai16@users.noreply.github.com>
2023-11-20 02:28:22 +0800

    add: netifly support and page layouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [tailwind()],
+  output: "server",
+  adapter: netlify()
 });
