bf5b630b38c35ba69ec226a2283927cddb585cbf
yichenlai16 <63837651+yichenlai16@users.noreply.github.com>
2023-11-20 14:05:17 +0800

    feat: undo add netlify support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify/functions";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [tailwind()],
-  output: "server",
-  adapter: netlify()
 });
