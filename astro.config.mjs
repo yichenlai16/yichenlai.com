4985d62c8094aeae3f88dca0b278b8f8b830bcd8
yichenlai16 <63837651+yichenlai16@users.noreply.github.com>
2023-11-22 14:57:43 +0800

    feat: add partytown and  google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [tailwind()],
+  integrations: [tailwind(), partytown()]
 });
